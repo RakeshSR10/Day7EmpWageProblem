1d4630b7c2cefdc77533ea760ee2ad265b071541
RakeshSR10 <rakeshsrking@gmail.com>
2021-04-05 08:26:02 +0530

    [Modified] added Switch case to EmployeeWage.java

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,11 +1,14 @@
 //Welcome to Daily Employee Wage Compute Program
-public class EmployeeWage {
+public class EmployeeWage 
+{
+	
+	//Constants
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_PER_RATE_HOUR = 20;
 
 	public static void main(String[] args) 
 	{
-		int IS_FULL_TIME = 2;
-		int IS_PART_TIME = 1;
-		int EMP_PER_RATE_HOUR = 20;
 
 		 //Variables
                 int empHrs=0;
@@ -19,14 +22,20 @@ public class EmployeeWage {
 			System.out.println("Employee is Absent");*/
 
 		//Computation and full time, part time
-		double empCheck = Math.floor(Math.random() *10) % 3;
-		if(empCheck == IS_PART_TIME)
-			empHrs=4;
-		else if(empCheck == IS_FULL_TIME)
-			empHrs=8;
-		else
-			empHrs=0;
+				//Computation using switch
+		int empCheck =(int) Math.floor(Math.random() *10) % 3;
 
+		switch(empCheck)
+		{
+		case IS_PART_TIME:
+			empHrs=4;
+			break;
+		case IS_FULL_TIME:
+			empHrs=8;
+			break;
+		default:
+			empHrs=0;
+		}
 		empWage = empHrs * EMP_PER_RATE_HOUR;
 		System.out.println("Employee wage = "+empWage);
 	}
